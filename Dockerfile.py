e16108ab3264c0e77cc86a4b55930172e7b7290d
SUPRIYA-VS <SUPRIYA.V@UNISYS.COM>
2024-12-13 11:05:11 +0530

    Add files via upload

--- Dockerfile.py
+++ Dockerfile.py
@@ -1,6 +1,6 @@
-FROM python
-# using python image from docker hub
-WORKDIR /ashucode
+FROM python 
+# using python image from docker hub 
+WORKDIR /ashucode 
 # creating and changing folder in docker image
 COPY automate.py /ashucode/
 CMD [ "python" , "automate.py" ]
